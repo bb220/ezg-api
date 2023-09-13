167200e91940103b3ca9bcda9dbee29bbd681216
Md Nuruddin <mdnuruddin043@gmail.com>
2023-09-14 01:23:04 +0600

    update roundlist api

--- src/core_app/controllers/round/index.js
+++ src/core_app/controllers/round/index.js
@@ -19,27 +19,106 @@ module.exports={
             next(e)
         }
     },
-    getRoundList:async(req,res,next)=>{
-        try{
-            let { page = 1, limit = 10 ,search} = req.query
-            limit = Number(limit)
-            page = Number(page)
-            const offset = (page - 1) * limit;            
-            let condition = {user:req.user._id, is_deleted:false}
-
-            if(search){
-                condition = {...condition,$or:[ {name:{ $regex: search, $options: 'si' }}] } 
+    getRoundList: async (req, res, next) => {
+        try {
+            let { page = 1, limit = 10, search } = req.query;
+            limit = Number(limit);
+            page = Number(page);
+            const offset = (page - 1) * limit;
+            let condition = { user: req.user._id, is_deleted: false };
+    
+            if (search) {
+                condition = { ...condition, $or: [{ name: { $regex: search, $options: 'si' }}] };
             }
-
-            const round_list = await Round.find(condition).sort({ createdAt: -1 }).populate("user","email").limit(limit).skip(offset).select("name course played_date createdAt updatedAt")
-
-            const count_docs = await Round.countDocuments(condition)
-            let total_pages = Math.ceil(count_docs / limit)
-            total_pages = total_pages === 0 ? 1 : total_pages
-
-            return res.send({ status : true, code :200, message:null, current_page:page, total_pages,total_rounds:count_docs, data:round_list })
-        }catch(e){
-            next(e)
+    
+            const round_list = await Round.aggregate([
+                { $match: condition },
+                {
+                    $lookup: {
+                        from: "courses",
+                        localField: "course",
+                        foreignField: "_id",
+                        as: "course"
+                    }
+                },
+                {
+                    $unwind: {
+                        path: "$course",
+                        preserveNullAndEmptyArrays: true
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "courseholes",
+                        localField: "course._id",
+                        foreignField: "course",
+                        pipeline: [
+                            { $match:
+                               { is_deleted: { $ne: true } }
+                            }
+                         ],
+                        as: "course_holes"
+                    }
+                },
+                {
+                    $addFields: {
+                        "course.total": { $sum: "$course_holes.par" }
+                    }
+                },
+                { $project: { course_holes: 0 } },
+                {
+                    $lookup: {
+                        from: "users", 
+                        localField: "user",
+                        foreignField: "_id",
+                        as: "user"
+                    }
+                },
+                {
+                    $unwind: "$user"
+                },
+                { $sort: { createdAt: -1 } },
+                { $skip: offset },
+                { $limit: limit },
+                {
+                    $project: {
+                        name: 1,
+                        user:{
+                            _id: "$user._id",
+                            email: "$user.email"
+                        },
+                        course: {
+                            _id: "$course._id",
+                            name: "$course.name",
+                            total: "$course.total"
+                        },
+                        played_date: 1,
+                        createdAt: 1,
+                        updatedAt: 1
+                    }
+                }
+            ]);
+    
+            const count_docs = await Round.countDocuments(condition);
+            let total_pages = Math.ceil(count_docs / limit);
+            total_pages = total_pages === 0 ? 1 : total_pages;
+            round_list.forEach((round)=>{
+                if(round.course._id===undefined){
+                    round.course=null
+                }
+            })
+    
+            return res.send({
+                status: true,
+                code: 200,
+                message: null,
+                current_page: page,
+                total_pages,
+                total_rounds: count_docs,
+                data: round_list
+            });
+        } catch (e) {
+            next(e);
         }
     },
     getSingleRound:async(req,res,next)=>{
